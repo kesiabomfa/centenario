9272936e3ce10c8b45d10e365ca8e5be1354e06a
kesiabomfa <52920939+kesiabomfa@users.noreply.github.com>
2026-08-24 10:00:35 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,10 +3,10 @@
 // Veja o SETUP.md para o passo a passo completo.
 
 export const firebaseConfig = {
-  apiKey: "COLOQUE_AQUI",
-  authDomain: "COLOQUE_AQUI.firebaseapp.com",
-  projectId: "COLOQUE_AQUI",
-  storageBucket: "COLOQUE_AQUI.appspot.com",
-  messagingSenderId: "COLOQUE_AQUI",
-  appId: "COLOQUE_AQUI"
+  apiKey: AIzaSyCeP-Crs2OoDdKCucnEQJBgJcEBhQN5-cE,
+  authDomain: centenario-363ef.firebaseapp.com,
+  projectId: centenario-363ef,
+  storageBucket: centenario-363ef.firebasestorage.app,
+  messagingSenderId: 924613179259,
+  appId: 1:924613179259:web:29a2bfe07126cb66442fde
 };
